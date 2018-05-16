30db2a0ee68ffd729ed591ee9053e007fb55bfb7
zhaoqy <zhaoqy@careland.com.cn>
2018-05-16 15:08:29 +0800

    update

--- app/src/main/java/com/zqy/canvasdemo/MainActivity.java
+++ app/src/main/java/com/zqy/canvasdemo/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -15,14 +17,16 @@ public class MainActivity extends AppCompatActivity {
 //    @BindView(R.id.tv_dian1)
 //    TextView tv_dian1;
 
+    ViewGroup viewGroup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        //findViewById(R.id.tv_dian0).setOnClickListener(this);
-        //findViewById(R.id.tv_dian1).setOnClickListener(this);
+        TextView dian0 = (TextView) findViewById(R.id.tv_dian0);
+        dian0.setTextSize(25);
     }
 
     @OnClick({R.id.tv_dian0, R.id.tv_dian1})
@@ -31,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
             case R.id.tv_dian0: {
                 Intent intent = new Intent(this, TestActivity.class);
                 startActivity(intent);
+
+
+
                 break;
             }
             case R.id.tv_dian1: {
